ac80b2afc78ea82df1528d2d4183e3d294441c66
ch <127966448+chnnasn@users.noreply.github.com>
2025-03-22 00:52:39 +0800

    add Json

---
+++ data/seniors.js
@@ -0,0 +1,671 @@
+const seniors = {
+    "艺术设计学院": [
+      {
+      "name": "陈敏棋",
+      "originalMajor": "数字媒体艺术设计",
+      "targetSchool": "广东财经大学",
+      "targetMajor": "广播电视编导",
+      "year": "2023"
+      },
+      {
+      "name": "何采滢",
+      "originalMajor": "视觉传达设计",
+      "targetSchool": "广东东软学院",
+      "targetMajor": "视觉传达设计",
+      "year": "2023"
+      },
+      {
+      "name": "张雍颖",
+      "originalMajor": "视觉传达设计",
+      "targetSchool": "广东技术师范大学",
+      "targetMajor": "工艺美术",
+      "year": "2023"
+      },
+      {
+      "name": "李景俊",
+      "originalMajor": "环境艺术设计",
+      "targetSchool": "广州商学院",
+      "targetMajor": "视觉传达设计",
+      "year": "2023"
+      },
+      {
+      "name": "罗泽敏",
+      "originalMajor": "环境艺术设计",
+      "targetSchool": "广东理工学院",
+      "targetMajor": "社会体育指导与管理",
+      "year": "2023"
+      },
+      {
+      "name": "郑文桐",
+      "originalMajor": "环境艺术设计",
+      "targetSchool": "韩山师范学院",
+      "targetMajor": "美术学",
+      "year": "2023"
+      },
+      {
+      "name": "谭姗姗",
+      "originalMajor": "游戏艺术设计",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "动画",
+      "year": "2023"
+      },
+      {
+      "name": "黎金文",
+      "originalMajor": "环境艺术设计",
+      "targetSchool": "广州南方学院",
+      "targetMajor": "艺术设计学",
+      "year": "2023"
+      },
+      {
+      "name": "苏锐玲",
+      "originalMajor": "环境艺术设计",
+      "targetSchool": "广州美术学院",
+      "targetMajor": "环境设计",
+      "year": "2023"
+      },
+      {
+      "name": "钟秋婷",
+      "originalMajor": "艺术设计",
+      "targetSchool": "广东技术师范大学",
+      "targetMajor": "工艺美术",
+      "year": "2023"
+      }
+      ],
+    "轻化工技术学院": [
+      {
+      "name": "万裕",
+      "originalMajor": "环境监测技术",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "工程管理",
+      "year": "2023"
+      },
+      {
+      "name": "张思瑜",
+      "originalMajor": "精细化工技术",
+      "targetSchool": "仲恺农业工程学院",
+      "targetMajor": "材料化学",
+      "year": "2023"
+      },
+      {
+      "name": "李淑云",
+      "originalMajor": "环境监测技术",
+      "targetSchool": "广东财经大学",
+      "targetMajor": "自然地理与资源环境",
+      "year": "2023"
+      },
+      {
+      "name": "苏信海",
+      "originalMajor": "分析检验技术",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "视觉传达设计",
+      "year": "2023"
+      },
+      {
+      "name": "曹怡玲",
+      "originalMajor": "化妆品经营与管理",
+      "targetSchool": "广州华立学院",
+      "targetMajor": "汉语言文学",
+      "year": "2023"
+      },
+      {
+      "name": "周玮",
+      "originalMajor": "印刷数字图文技术",
+      "targetSchool": "广州华立学院",
+      "targetMajor": "汉语言文学",
+      "year": "2023"
+      },
+      {
+      "name": "余博格",
+      "originalMajor": "食品检验检测技术",
+      "targetSchool": "广东科技学院",
+      "targetMajor": "工商管理",
+      "year": "2023"
+      },
+      {
+      "name": "刘晓君",
+      "originalMajor": "印刷数字图文技术",
+      "targetSchool": "广州新华学院",
+      "targetMajor": "网络与新媒体",
+      "year": "2023"
+      },
+      {
+      "name": "谢铱桁",
+      "originalMajor": "化妆品经营与管理",
+      "targetSchool": "广州软件学院",
+      "targetMajor": "市场营销",
+      "year": "2023"
+      },
+      {
+      "name": "李泳茹",
+      "originalMajor": "印刷数字图文技术",
+      "targetSchool": "广州软件学院",
+      "targetMajor": "英语",
+      "year": "2023"
+      },
+      {
+      "name": "邹夏菁",
+      "originalMajor": "化妆品经营与管理",
+      "targetSchool": "广东外语外贸大学南国商学院",
+      "targetMajor": "市场营销",
+      "year": "2023"
+      },
+      {
+      "name": "陈艺",
+      "originalMajor": "精细化工技术",
+      "targetSchool": "仲恺农业工程学院",
+      "targetMajor": "化学工程与工艺",
+      "year": "2023"
+      },
+      {
+      "name": "吴思苑",
+      "originalMajor": "现代造纸技术",
+      "targetSchool": "广州软件学院",
+      "targetMajor": "人力资源管理",
+      "year": "2023"
+      }
+      ],
+    "信息技术学院":[
+      {
+      "name": "黄乐诗",
+      "originalMajor": "数字媒体技术",
+      "targetSchool": "广东东软学院",
+      "targetMajor": "软件工程",
+      "year": "2023"
+      },
+      {
+      "name": "梁俊熙",
+      "originalMajor": "云计算技术应用",
+      "targetSchool": "广州软件学院",
+      "targetMajor": "数字媒体技术",
+      "year": "2023"
+      },
+      {
+      "name": "康荧",
+      "originalMajor": "软件技术",
+      "targetSchool": "湛江科技学院",
+      "targetMajor": "汉语言文学",
+      "year": "2023"
+      },
+      {
+      "name": "李玮然",
+      "originalMajor": "云计算技术应用",
+      "targetSchool": "广州南方学院",
+      "targetMajor": "工商管理",
+      "year": "2023"
+      },
+      {
+      "name": "连光鑫",
+      "originalMajor": "通信工程设计与监理",
+      "targetSchool": "珠海科技学院",
+      "targetMajor": "自动化",
+      "year": "2023"
+      },
+      {
+      "name": "梁智坚",
+      "originalMajor": "计算机应用技术",
+      "targetSchool": "广东科技学院",
+      "targetMajor": "软件工程",
+      "year": "2023"
+      },
+      {
+      "name": "龚骏毅",
+      "originalMajor": "数字媒体技术",
+      "targetSchool": "广东东软学院",
+      "targetMajor": "电子商务",
+      "year": "2023"
+      },
+      {
+      "name": "张凯文",
+      "originalMajor": "数字媒体技术",
+      "targetSchool": "广东科技学院",
+      "targetMajor": "英语",
+      "year": "2023"
+      },
+      {
+      "name": "邓惠娜",
+      "originalMajor": "数字媒体技术",
+      "targetSchool": "广州新华学院",
+      "targetMajor": "数字媒体技术",
+      "year": "2023"
+      },
+      {
+      "name": "赖浩瑜",
+      "originalMajor": "现代通信技术",
+      "targetSchool": "珠海科技学院",
+      "targetMajor": "旅游管理",
+      "year": "2023"
+      }
+      ],
+    "机电技术学院": [
+      {
+      "name": "周睿文",
+      "originalMajor": "机械制造及自动化",
+      "targetSchool": "广州科技职业技术大学",
+      "targetMajor": "机械设计制造及自动化",
+      "year": "2023"
+      },
+      {
+      "name": "陈仕越",
+      "originalMajor": "电气自动化技术",
+      "targetSchool": "广州城市理工学院",
+      "targetMajor": "车辆工程",
+      "year": "2023"
+      },
+      {
+      "name": "卓新鹏",
+      "originalMajor": "机械制造及自动化",
+      "targetSchool": "广东外语外贸大学南国商学院",
+      "targetMajor": "计算机科学与技术",
+      "year": "2023"
+      },
+      {
+      "name": "许彼鞍",
+      "originalMajor": "机电一体化技术",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "软件工程",
+      "year": "2023"
+      },
+      {
+      "name": "黄文锋",
+      "originalMajor": "机械制造及自动化",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "机器人工程",
+      "year": "2023"
+      },
+      {
+      "name": "赖文滨",
+      "originalMajor": "电气自动化技术",
+      "targetSchool": "东莞城市学院",
+      "targetMajor": "环境工程",
+      "year": "2023"
+      },
+      {
+      "name": "叶骏耀",
+      "originalMajor": "机电一体化技术",
+      "targetSchool": "广州应用科技学院",
+      "targetMajor": "机械电子工程",
+      "year": "2023"
+      },
+      {
+      "name": "刘奕涛",
+      "originalMajor": "机械制造及自动化",
+      "targetSchool": "广东石油化工学院",
+      "targetMajor": "材料成型及控制工程",
+      "year": "2023"
+      },
+      {
+      "name": "李佳积",
+      "originalMajor": "机电一体化技术",
+      "targetSchool": "广东石油化工学院",
+      "targetMajor": "过程装备与控制工程",
+      "year": "2023"
+      }
+      ],
+    "汽车技术学院": [
+      {
+      "name": "黄强",
+      "originalMajor": "新能源汽车技术",
+      "targetSchool": "东莞理工学院",
+      "targetMajor": "材料科学与工程",
+      "year": "2023"
+      },
+      {
+      "name": "林倬均",
+      "originalMajor": "汽车智能技术",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "电气工程及其自动化",
+      "year": "2023"
+      },
+      {
+      "name": "杨承源",
+      "originalMajor": "汽车检测与维修技术",
+      "targetSchool": "湛江科技学院",
+      "targetMajor": "汽车服务工程",
+      "year": "2023"
+      },
+      {
+      "name": "李海栋",
+      "originalMajor": "汽车检测与维修技术",
+      "targetSchool": "广州商学院",
+      "targetMajor": "电子商务",
+      "year": "2023"
+      },
+      {
+      "name": "黎赓文",
+      "originalMajor": "制冷与空调技术",
+      "targetSchool": "仲恺农业工程学院",
+      "targetMajor": "能源与动力工程",
+      "year": "2023"
+      },
+      {
+      "name": "梁立业",
+      "originalMajor": "制冷与空调技术",
+      "targetSchool": "广州科技职业技术大学",
+      "targetMajor": "机械设计制造及自动化",
+      "year": "2023"
+      },
+      {
+      "name": "袁博跃",
+      "originalMajor": "汽车技术服务与营销",
+      "targetSchool": "珠海科技学院",
+      "targetMajor": "车辆工程",
+      "year": "2023"
+      },
+      {
+      "name": "彭展博",
+      "originalMajor": "汽车智能技术",
+      "targetSchool": "韶关学院",
+      "targetMajor": "数学与应用数学",
+      "year": "2023"
+      },
+      {
+      "name": "黄玺",
+      "originalMajor": "新能源汽车技术",
+      "targetSchool": "仲恺农业工程学院",
+      "targetMajor": "能源与动力工程",
+      "year": "2023"
+      }
+      ],
+    "生态环境技术学院": [
+      {
+      "name": "刘盛岚",
+      "originalMajor": "城乡规划",
+      "targetSchool": "广州商学院",
+      "targetMajor": "智能科学与技术",
+      "year": "2023"
+      },
+      {
+      "name": "林慧燕",
+      "originalMajor": "城乡规划",
+      "targetSchool": "广州软件学院",
+      "targetMajor": "行政管理",
+      "year": "2023"
+      },
+      {
+      "name": "罗捷",
+      "originalMajor": "环境工程技术",
+      "targetSchool": "东莞城市学院",
+      "targetMajor": "工商管理",
+      "year": "2023"
+      },
+      {
+      "name": "朱秋华",
+      "originalMajor": "氢能技术应用",
+      "targetSchool": "广州应用科技学院",
+      "targetMajor": "英语专业",
+      "year": "2023"
+      },
+      {
+      "name": "谢芷茵",
+      "originalMajor": "园林工程技术",
+      "targetSchool": "湛江科技学院",
+      "targetMajor": "园林",
+      "year": "2023"
+      },
+      {
+      "name": "王紫芊",
+      "originalMajor": "城乡规划",
+      "targetSchool": "广州新华学院",
+      "targetMajor": "社会工作",
+      "year": "2023"
+      },
+      {
+      "name": "戴穗立",
+      "originalMajor": "园林工程技术",
+      "targetSchool": "广州商学院",
+      "targetMajor": "法学",
+      "year": "2023"
+      },
+      {
+      "name": "梁乐瑶",
+      "originalMajor": "环境工程技术",
+      "targetSchool": "广州应用科技学院",
+      "targetMajor": "通信工程",
+      "year": "2023"
+      },
+      {
+      "name": "陈卓琳",
+      "originalMajor": "建筑装饰材料技术",
+      "targetSchool": "广州工商学院",
+      "targetMajor": "食品质量与安全",
+      "year": "2023"
+      },
+      {
+      "name": "许勤生",
+      "originalMajor": "建筑装饰材料技术",
+      "targetSchool": "广州新华学院",
+      "targetMajor": "会计学",
+      "year": "2023"
+      },
+      {
+      "name": "彭晓锋",
+      "originalMajor": "环境工程技术",
+      "targetSchool": "广东工商职业技术大学",
+      "targetMajor": "学前教育",
+      "year": "2023"
+      },
+      {
+      "name": "吴其睿",
+      "originalMajor": "园林工程技术",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "人力资源管理",
+      "year": "2023"
+      },
+      {
+      "name": "朱大全",
+      "originalMajor": "园林工程技术",
+      "targetSchool": "五邑大学",
+      "targetMajor": "社会工作",
+      "year": "2023"
+      },
+      {
+      "name": "郑梓凌",
+      "originalMajor": "园林工程技术",
+      "targetSchool": "广州科技职业技术大学",
+      "targetMajor": "数字媒体技术",
+      "year": "2023"
+      }
+      ],
+    "食品与生物技术学院": [
+      {
+      "name": "郑梓凌",
+      "originalMajor": "园林工程技术",
+      "targetSchool": "广州科技职业技术大学",
+      "targetMajor": "数字媒体技术",
+      "year": "2023"
+      },
+      {
+      "name": "张尔瑾",
+      "originalMajor": "食品质量与安全",
+      "targetSchool": "仲恺农业工程学院",
+      "targetMajor": "食品质量与安全",
+      "year": "2023"
+      },
+      {
+      "name": "梁金华",
+      "originalMajor": "食品营养与健康",
+      "targetSchool": "广州城市理工学院",
+      "targetMajor": "物流工程",
+      "year": "2023"
+      },
+      {
+      "name": "林楚涵",
+      "originalMajor": "食品质量与安全",
+      "targetSchool": "韶关学院",
+      "targetMajor": "汉语言文学",
+      "year": "2023"
+      },
+      {
+      "name": "吴宇隆",
+      "originalMajor": "药品生产技术",
+      "targetSchool": "广州新华学院",
+      "targetMajor": "网络与新媒体",
+      "year": "2023"
+      },
+      {
+      "name": "黄惠津",
+      "originalMajor": "食品营养与健康",
+      "targetSchool": "惠州学院",
+      "targetMajor": "社会工作",
+      "year": "2023"
+      },
+      {
+      "name": "莫嘉琦",
+      "originalMajor": "食品智能加工技术",
+      "targetSchool": "肇庆学院",
+      "targetMajor": "食品科学与工程",
+      "year": "2023"
+      },
+      {
+      "name": "洪满玉",
+      "originalMajor": "食品智能加工技术",
+      "targetSchool": "广州华商学院",
+      "targetMajor": "人力资源管理",
+      "year": "2023"
+      },
+      {
+      "name": "肖强华",
+      "originalMajor": "食品智能加工技术",
+      "targetSchool": "广州应用科技学院",
+      "targetMajor": "电子信息工程",
+      "year": "2023"
+      },
+      {
+      "name": "李依琳",
+      "originalMajor": "食品质量与安全",
+      "targetSchool": "广州工商学院",
+      "targetMajor": "食品营养与检验教育",
+      "year": "2023"
+      },
+      {
+      "name": "梁俊贤",
+      "originalMajor": "化工生物技术",
+      "targetSchool": "东莞城市学院",
+      "targetMajor": "人文地理与城乡规划",
+      "year": "2023"
+      }
+      ],
+    "管理学院": [
+      {
+      "name": "黄钰樱",
+      "originalMajor": "社会工作",
+      "targetSchool": "韩山师范学院",
+      "targetMajor": "烹饪与营养教育（职教师资）",
+      "year": "2023"
+      },
+      {
+      "name": "梁志睿",
+      "originalMajor": "社区管理与服务",
+      "targetSchool": "广州南方学院",
+      "targetMajor": "人力资源管理",
+      "year": "2023"
+      },
+      {
+      "name": "张时豪",
+      "originalMajor": "旅游管理",
+      "targetSchool": "广州科技职业技术大学",
+      "targetMajor": "物流工程技术",
+      "year": "2023"
+      },
+      {
+      "name": "陈佳欣",
+      "originalMajor": "酒店管理与数字化运营",
+      "targetSchool": "广东东软学院",
+      "targetMajor": "财务管理",
+      "year": "2023"
+      },
+      {
+      "name": "曾倩",
+      "originalMajor": "社区管理与服务",
+      "targetSchool": "广州新华学院",
+      "targetMajor": "健康服务与管理",
+      "year": "2023"
+      }
+      ],
+    "应用外语学院": [
+      {
+      "name": "陈佳媛",
+      "originalMajor": "应用英语",
+      "targetSchool": "韶关学院",
+      "targetMajor": "商务英语",
+      "year": "2023"
+      },
+      {
+      "name": "吴燕萍",
+      "originalMajor": "商务英语",
+      "targetSchool": "韩山师范学院",
+      "targetMajor": "人力资源管理",
+      "year": "2023"
+      },
+      {
+      "name": "曾璐琼",
+      "originalMajor": "商务日语",
+      "targetSchool": "韩山师范学院",
+      "targetMajor": "法学",
+      "year": "2023"
+      },
+      {
+      "name": "林杰婷",
+      "originalMajor": "国际商务",
+      "targetSchool": "广东白云学院",
+      "targetMajor": "社会工作",
+      "year": "2023"
+      },
+      {
+      "name": "林奕瑜",
+      "originalMajor": "应用德语",
+      "targetSchool": "广州应用科技学院",
+      "targetMajor": "法学",
+      "year": "2023"
+      },
+      {
+      "name": "杨梓淇",
+      "originalMajor": "应用外语学院",
+      "targetSchool": "湛江科技学院",
+      "targetMajor": "计算机科学与技术",
+      "year": "2023"
+      },
+      {
+      "name": "程茶",
+      "originalMajor": "应用外语学院",
+      "targetSchool": "肇庆学院",
+      "targetMajor": "学前教育",
+      "year": "2023"
+      },
+      {
+      "name": "余林涛",
+      "originalMajor": "应用外语学院",
+      "targetSchool": "湛江科技学院",
+      "targetMajor": "汉语言文学",
+      "year": "2023"
+      },
+      {
+      "name": "黄媛娜",
+      "originalMajor": "应用外语学院",
+      "targetSchool": "广州科技职业技术大学",
+      "targetMajor": "数字印刷工程",
+      "year": "2023"
+      },
+      {
+      "name": "卓裕钦",
+      "originalMajor": "应用外语学院",
+      "targetSchool": "珠海科技学院",
+      "targetMajor": "金融学",
+      "year": "2023"
+      },
+      {
+      "name": "欧富灏",
+      "originalMajor": "应用外语学院",
+      "targetSchool": "广州应用科技学院",
+      "targetMajor": "行政管理",
+      "year": "2023"
+      },
+      {
+      "name": "范丽静",
+      "originalMajor": "应用外语学院",
+      "targetSchool": "广东外语外贸大学南国商学院",
+      "targetMajor": "德语",
+      "year": "2023"
+      }
+      ]
+  };
